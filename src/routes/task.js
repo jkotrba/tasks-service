c1286bec1c4dad20dfa2b5bbe817b970cd4019d4
jkotrba <jerome.kotrba@gmail>
2018-05-02 20:32:32 -0700

    refactored to use async/await

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,34 +1,40 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import uuid from 'uuid/v4';
-import task from '../db/models/task';
+import express from "express";
+import bodyParser from "body-parser";
+import uuid from "uuid/v4";
+import task from "../db/models/task";
 
 const router = express.Router();
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.get('/', async (req, res) => {
-  task.find({}, (err, tasks) => {
-    if(err) {
-      return res.status(500).json({error: {message: 'An error occurred loading tasks'}});
-    }
-    res.json({data: tasks});
-  });
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await task.find({});
+    res.json({ data: tasks });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: { message: "An error occurred loading tasks" } });
+  }
 });
 
-router.post('/', async (req, res) => {
-  const newTask = {
-    taskId: uuid(),
-    title: req.body.data.title,
-    description: req.body.data.description,
-    createdDate: new Date()
-  };
-  task.create(newTask, (err, created) => {
-    if(err) {
-      return res.status(500).json({error: {message: 'An error creating a task'}});
-    }
-    res.json({data: created});
-  });
+router.post("/", async (req, res) => {
+  try {
+    let { title, description } = req.body.data;
+
+    const newTask = {
+      taskId: uuid(),
+      title: title,
+      description: description,
+      createdDate: new Date()
+    };
+    const created = await task.create(newTask);
+    res.json({ data: created });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: { message: "An error creating a task" } });
+  }
 });
 
 export default router;
