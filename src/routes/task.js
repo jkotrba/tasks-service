08247285adeaa2df294303c9576c687f3f52dbf7
jkotrba <jerome.kotrba@gmail>
2018-05-02 18:49:35 -0700

    add task route, add mongoose

---
+++ src/routes/task.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  res.send('Task!');
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+export default router;
