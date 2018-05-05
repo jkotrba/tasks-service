1a207ffefa6a0f707e3731992116dda9be9e3be5
jkotrba <jerome.kotrba@gmail>
2018-05-05 12:02:37 -0700

    added more route handlers

--- src/routes/task.js
+++ src/routes/task.js
@@ -7,6 +7,22 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+router.get('/:id', async(req, res) => {
+  const id = req.params.id;
+  try {
+    const foundTask = await task.findOne({taskId: id});
+    if(foundTask) {
+      res.json({ data: foundTask });
+    }
+    res.status(404).json({message: 'NOT FOUND'});
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ error: { message: `An error occurred loading task ${id}` } });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
     const tasks = await task.find({});
@@ -17,6 +33,7 @@ router.get("/", async (req, res) => {
       .json({ error: { message: "An error occurred loading tasks" } });
   }
 });
+
 
 router.post("/", async (req, res) => {
   try {
@@ -36,5 +53,48 @@ router.post("/", async (req, res) => {
       .json({ error: { message: "An error creating a task" } });
   }
 });
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    let { title, description } = req.body.data;
+    const query = { taskId: id };
+    const updateTask =  {
+      taskId: id,
+      title: title,
+      description: description
+    }
+
+    const updated = await task.findOneAndUpdate(query, updateTask, { new: true });
+    if(updated) {
+      res.json( {data: updated });
+    }
+    res.status(404).json({message: 'NOT FOUND'});
+  }
+  catch(err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ error: { message: `An error occurred updating task ${id}` } });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleted = await task.findOneAndRemove({taskId: id});
+    if(deleted) {
+      res.status(200).send();
+    }
+    res.status(404).json({message: 'NOT FOUND'});
+  }
+  catch(err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ error: { message: `An error occurred deleting task ${id}` } });
+  }
+});
+
 
 export default router;
